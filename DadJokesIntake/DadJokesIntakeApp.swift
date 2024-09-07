43a9c832be04b504c9cfbf4e00ebef747ea7d9e5
Annurdien Rasyid <annurdien.dev@gmail.com>
2024-09-07 13:57:07 +0700

    Initial Commit

---
+++ DadJokesIntake/DadJokesIntakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesIntakeApp.swift
+//  DadJokesIntake
+//
+//  Created by Annurdien Rasyid on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesIntakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
